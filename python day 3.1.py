9dcb6660c1eb18eebeb848199b9e1c99fb416831
Punemprasanth <124040860+Punemprasanth@users.noreply.github.com>
2023-02-01 13:44:43 +0530

    Add files via upload

---
+++ python day 3.1.py
@@ -0,0 +1,39 @@
+num=(1,2,3,4,5,6,7,8,9,10)
+print(num)
+print("first element in the list",num[0])
+print(num[2:5])
+print(num[1:9])
+print(num[2:7])
+print(num[: : 2])
+print(num[1: :3])
+
+          
+
+
+
+
+num=[1,'a',"abc",[2,3,4],5,6]
+print(num)
+
+
+
+
+num=[100,2,3,6,-8]
+print(min(num))
+print(sum(num))
+
+
+
+
+num=[1,2,3,4,5,6,7,8,10]
+print(sum(num))
+
+
+
+num=[12,-2,-33,84,5,6,87,8,99,10]
+print(sorted(num))
+print(any(num))
+
+num(12,-2,-33,84)
+num.append(0)
+print(num)
